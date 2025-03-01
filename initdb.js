345243e2dafe8e5cf85f3a0257926a824fa6f65f
mirzamar <132921031+mirzamar@users.noreply.github.com>
2025-03-01 17:53:12 +0100

    Datenbank angebunden

--- initdb.js
+++ initdb.js
@@ -6,79 +6,78 @@ const format = require('pg-format');
 
 initdb.get('/', async(req, res) => {
 
-    // Anlegen der Tabelle members
+    // Anlegen der Tabelle transaktion
     let query = `
-            DROP TABLE IF EXISTS members;
-            CREATE TABLE members(id serial PRIMARY KEY, firstname VARCHAR(50), lastname VARCHAR(50), email VARCHAR(50));
+            DROP TABLE IF EXISTS transaktion;
+            CREATE TABLE transaktion(transaktionsid serial PRIMARY KEY, transaktionstyp VARCHAR(50), beschreibung VARCHAR(50), betrag DOUBLE PRECISION, kategorieid INTEGER, datum DATE);
+            `;
+
+    // Anlegen der Tabelle kategorie
+    let query2 = `
+            DROP TABLE IF EXISTS kategorie;
+            CREATE TABLE kategorie(kategorieid serial PRIMARY KEY, name VARCHAR(50), wichtigkeit BOOL);
             `;
 
     try {
         await client.query(query)
+        await client.query(query2)
         console.log("Table created successfully ...")
     } catch (err) {
         console.log(err)
     }
 
-    // Befüllen der Tabelle members mit 50 Einträgen
-    const values = [
-        ["Catherine", "Williams", "cwilliamsl@360.cn"],
-        ["Adam", "Anderson", "aanderson8@google.fr"],
-        ["Susan", "Andrews", "sandrewsn@google.co.jp"],
-        ["Catherine", "Andrews", "candrewsp@noaa.gov"],
-        ["Alan", "Bradley", "abradley1c@globo.com"],
-        ["Anne", "Brooks", "abrooks16@bravesites.com"],
-        ["Russell", "Brown", "rbrownq@nifty.com"],
-        ["Ryan", "Burton", "rburton18@foxnews.com"],
-        ["Roy", "Campbell", "rcampbell1@geocities.com"],
-        ["Russell", "Campbell", "rcampbell17@eventbrite.com"],
-        ["Bonnie", "Coleman", "bcoleman11@fc2.com"],
-        ["Ernest", "Coleman", "ecoleman15@businessweek.com"],
-        ["Richard", "Cruz", "rcruz7@unc.edu"],
-        ["Sean", "Cruz", "scruz10@answers.com"],
-        ["Rebecca", "Cunningham", "rcunninghamd@mac.com"],
-        ["Margaret", "Evans", "mevansh@pcworld.com"],
-        ["Jeffrey", "Ford", "jford14@cnet.com"],
-        ["Andrea", "Gardner", "agardnerv@woothemes.com"],
-        ["Deborah", "George", "dgeorge6@furl.net"],
-        ["Sean", "Gibson", "sgibsony@alexa.com"],
-        ["Virginia", "Graham", "vgrahamk@aol.com"],
-        ["Steven", "Hamilton", "shamiltonu@state.tx.us"],
-        ["Virginia", "Hawkins", "vhawkinsf@ehow.com"],
-        ["Edward", "Hicks", "ehicksc@pcworld.com"],
-        ["Mark", "Johnson", "mjohnsonj@hostgator.com"],
-        ["Ruth", "Jordan", "rjordan1a@smugmug.com"],
-        ["Antonio", "Kim", "akim4@odnoklassniki.ru"],
-        ["Jennifer", "Marshall", "jmarshallt@gnu.org"],
-        ["Eric", "Matthews", "ematthews5@independent.co.uk"],
-        ["Raymond", "Mcdonald", "rmcdonald2@ihg.com"],
-        ["Eric", "Miller", "emillere@creativecommons.org"],
-        ["Jonathan", "Morales", "jmoralesa@ovh.net"],
-        ["Marie", "Morgan", "mmorganb@cloudflare.com"],
-        ["Amanda", "Nelson", "anelson13@indiatimes.com"],
-        ["Lisa", "Olson", "lolsonr@telegraph.co.uk"],
-        ["Alice", "Ortiz", "aortizw@histats.com"],
-        ["Peter", "Phillips", "pphillipss@1688.com"],
-        ["Matthew", "Porter", "mporter9@europa.eu"],
-        ["Tammy", "Ray", "trayx@weather.com"],
-        ["Mark", "Richardson", "mrichardson1d@ihg.com"],
-        ["Joan", "Roberts", "jroberts12@alibaba.com"],
-        ["Kathleen", "Rose", "kroseg@pinterest.com"],
-        ["Steve", "Sanders", "ssanders1b@wikispaces.com"],
-        ["Shirley", "Scott", "sscottm@macromedia.com"],
-        ["Lillian", "Stephens", "lstephens19@hugedomains.com"],
-        ["Nicole", "Thompson", "nthompson3@admin.ch"],
-        ["Marie", "Thompson", "mthompsonz@yelp.com"],
-        ["Alan", "Vasquez", "avasquezo@miibeian.gov.cn"],
-        ["Mildred", "Watkins", "mwatkins0@miibeian.gov.cn"],
-        ["Eugene", "Williams", "ewilliamsi@deliciousdays.com"]
+    // Befüllen der Tabelle transaktion mit 50 Einträgen
+    const values_transaktion = [
+        ["Ausgabe", "Schampoo und Duschgel", 3.01, 0, "2025-01-01"],
+        ["Ausgabe", "Fahrradmantel", 19.99, 1, "2025-01-01"],
+        ["Ausgabe", "Iboprophen", 10.00, 2, "2025-01-01"],
+        ["Ausgabe", "Zugticket", 50.00, 3, "2025-01-10"],
+        ["Ausgabe", "Duschschlauch", 9.99, 4, "2025-01-10"],
+        ["Ausgabe", "Konzertticket", 15.99, 5, "2025-01-10"],
+        ["Ausgabe", "Halleneintritt", 12.50, 6, "2025-01-10"],
+        ["Ausgabe", "Hotel", 59.99, 7, "2025-01-22"],
+        ["Ausgabe", "Einkauf Rewe", 30.99, 8, "2025-01-22"],
+        ["Ausgabe", "Miete Januar", 750.00, 9, "2025-01-22"],
+        ["Ausgabe", "Krankenversicherung Januar", 200.00, 10, "2025-01-23"],
+        ["Einnahme", "Gehalt", 1200.00, 18, "2025-01-24"],
+        ["Einnahme", "Wohngeld", 100.00, 18, "2025-01-24"],
     ];
+
+    // Befüllen der Tabelle kategorie mit relevanten Einträgen
+    const values_kategorie = [
+        ["Kosmetik", false],
+        ["Reperaturen", true],
+        ["Medikamente", true],
+        ["Transport", true],
+        ["Wohnung", false],
+        ["Ausgehen", false],
+        ["Klettern", false],
+        ["Urlaub", false],
+        ["Essen", true],
+        ["Miete", true],
+        ["Krankenversicherung", true],
+        ["GEZ", true],
+        ["Strom", true],
+        ["Nebenkosten", true],
+        ["Versicherung", true],
+        ["Anlage", false],
+        ["Mitgliedschaften", false],
+        ["Sonstiges", false],
+        ["Job", false],
+    ];
+
+
+
     // hierfuer muss pg-format installiert werden (wegen %L):
-    const paramquery = format('INSERT INTO members(firstname, lastname, email) VALUES %L RETURNING *', values);
+    const paramquery = format('INSERT INTO transaktion(transaktionstyp, beschreibung, betrag, kategorieid, datum) VALUES %L RETURNING *', values_transaktion);
+
+    const paramquery2 = format('INSERT INTO kategorie(name, wichtigkeit) VALUES %L RETURNING *', values_kategorie);
 
 
     try {
         const result = await client.query(paramquery)
-        console.log("50 members inserted ...")
+        const result2 = await client.query(paramquery2)
+        console.log("transaktionen hinzugefuegt ...")
         res.status(200)
         res.send(result.rows)
     } catch (err) {
