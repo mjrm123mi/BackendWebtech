7a2c5b4162a57a134686dedbdcbfc600503c3c98
mirzamar <132921031+mirzamar@users.noreply.github.com>
2025-03-06 21:52:07 +0100

    Datum repariert

--- db.js
+++ db.js
@@ -1,5 +1,12 @@
 const pg = require('pg');
 
+//die Zeile hier drunter hinzugefuegt damit das Datum richtig angezeigt wird:
+pg.types.setTypeParser(1082, value => value);
+//In der DB sind die Datum als reines Datum. Wenn Node.js sich das Datum holt, wandelt
+// es in eine JavascriptDatum um. Dieses Date hat schon Zeitstunden. Deswegen wird es so angezeigt.
+// In Zeile 4 wird dem pg modul (eine libary für node.js die mit der DBpostgreSQL kommuniziert)
+// wird gesagt dass es das Datum so lassen soll.
+
 const client = new pg.Client({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
